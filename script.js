b99cdbe9db762f4faa92deec97bf7bfa2083009c
unknown <j.kiwinicki@wp.pl>
2021-07-13 19:23:27 +0200

    fixes and changes

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const workTimesTitle = document.querySelector('#work-times-title');
-const workTimesList = document.querySelector('#work-times-list');
-const workTimesArrow = workTimesTitle.querySelector('.bi-caret-down-fill');
+(() => {
+	const workTimesTitle = document.querySelector('#work-times-title');
+	const workTimesList = document.querySelector('#work-times-list');
+	const workTimesArrow = workTimesTitle.querySelector('.bi-caret-down-fill');
 
-workTimesTitle.addEventListener('click', toggleWorkTimesList);
+	workTimesTitle.addEventListener('click', toggleWorkTimesList);
 
-function toggleWorkTimesList() {
-	if (workTimesList.style.maxHeight) {
-		workTimesList.style.maxHeight = null;
+	function toggleWorkTimesList() {
+		if (workTimesList.style.maxHeight) {
+			workTimesList.style.maxHeight = null;
+			workTimesTitle.setAttribute('aria-expanded', 'false');
 
-		workTimesArrow.style.transition = 'transform 0.3s';
-		workTimesArrow.style.transform = 'rotate(0deg)';
-	} else {
-		workTimesList.style.maxHeight = workTimesList.scrollHeight + 'px';
+			workTimesArrow.style.transition = 'transform 0.3s';
+			workTimesArrow.style.transform = 'rotate(0deg)';
+		} else {
+			workTimesList.style.maxHeight = workTimesList.scrollHeight + 'px';
+			workTimesTitle.setAttribute('aria-expanded', 'true');
 
-		workTimesArrow.style.transition = 'transform 0.3s';
-		workTimesArrow.style.transform = 'rotate(180deg)';
+			workTimesArrow.style.transition = 'transform 0.3s';
+			workTimesArrow.style.transform = 'rotate(180deg)';
+		}
 	}
-}
+})();
