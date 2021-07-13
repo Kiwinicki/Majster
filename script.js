c53907fc267b79707a8bdc1b6dd7c911dab1f0d0
unknown <j.kiwinicki@wp.pl>
2021-07-13 10:45:33 +0200

    added WAI-ARIA and microdata

--- script.js
+++ script.js
@@ -3,14 +3,21 @@ const workTimesList = document.querySelector('#work-times-list');
 const workTimesArrow = workTimesTitle.querySelector('.bi-caret-down-fill');
 
 workTimesTitle.addEventListener('click', toggleWorkTimesList);
+workTimesTitle.addEventListener(
+	'keyup',
+	(e) => e.key == 'Enter' && toggleWorkTimesList()
+);
+
 function toggleWorkTimesList() {
 	if (workTimesList.style.maxHeight) {
 		workTimesList.style.maxHeight = null;
+		workTimesTitle.setAttribute('aria-expanded', 'false');
 
 		workTimesArrow.style.transition = 'transform 0.3s';
 		workTimesArrow.style.transform = 'rotate(0deg)';
 	} else {
 		workTimesList.style.maxHeight = workTimesList.scrollHeight + 'px';
+		workTimesTitle.setAttribute('aria-expanded', 'true');
 
 		workTimesArrow.style.transition = 'transform 0.3s';
 		workTimesArrow.style.transform = 'rotate(180deg)';
